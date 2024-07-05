d6a64e751951bcfb05f5816c7fa6b63c737e21da
n-i-w-o <chengq95588@163.com>
2024-07-05 14:49:32 +0800

    Add files via upload

---
+++ Dijkstra.cpp
@@ -0,0 +1,79 @@
+#include"TripTable.h"
+#include<iostream>
+#include<vector>
+#include"Graph.h"
+
+void Graph::Dijkstra(Node** g, int src, int dest, int tripType, int method)
+{
+    //第一部分，将图等效为int图
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+            if (g[i][j].Sdata != NULL)
+            {
+                g[i][j].choose(tripType, method);//赋予权值
+            }
+        }
+    }
+
+    //第二部分，主体算法
+    int v = src;//简化变量名称
+    int dist[V];
+    int path[V];
+    int S[V];
+    for (int i = 0; i < V; i++)
+    {
+        dist[i] = g[v][i].data;
+        S[i] = 0;
+        if (g[v][i].data != 0 && g[v][i].data < INF)
+            path[i] = v;
+        else
+            path[i] = -1;
+    }
+    S[v] = 1;
+    int mindus = -1;
+    int u = -1;
+    for (int i = 0; i < V - 1; i++)
+    {
+        mindus = INF;
+        for (int j = 0; j < V; j++)
+        {
+            if (S[j] == 0 && dist[j] < mindus)
+            {
+                u = j;
+                mindus = dist[j];
+            }
+        }
+        S[u] = 1;
+        for (int j = 0; j < V; j++)
+        {
+            if (S[j] == 0)
+                if (g[u][j].data < INF && g[u][j] + dist[u] < dist[j])
+                {
+                    dist[j] = g[u][j] + dist[u];
+                    path[j] = u;
+                }
+        }
+    }
+
+    //第三部分，打印路径和结果，根据前端不同改变
+    int i = dest;
+    if (S[i] == 1 && i != v)
+    {
+        vector<int> apath;
+        cout << dist[i];
+        apath.push_back(i);
+        int pre = path[i];
+        while (pre != v)
+        {
+            apath.push_back(i);
+            pre = path[pre];
+        }
+        cout << v;
+        for (int k = apath.size() - 1; k >= 0; k--)
+            cout << apath[k];
+    }
+    else
+        cout << "NOPath";
+}
