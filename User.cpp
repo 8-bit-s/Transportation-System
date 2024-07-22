578dce31c05129ab75c475f7c00c1c761627dd29
wsd0721 <2023302112020@whu.edu.cn>
2024-07-22 20:07:45 +0800

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -4,16 +4,6 @@
 #include"TripTable.h"
 using std::string;
 
-bool isCity(string name1) {
-	int len = name1.length();
-	for (int i = 0; i < len; i++) {
-		if (name1[i] > 'z' || name1[i] < 'a') {
-			return 0;
-		}
-	}
-	return 1;
-}
-
 bool Admin::logIn(string key1) {
 	if (key1 == KEY) {
 		isOnline = 1;
@@ -29,42 +19,58 @@ void Admin::logOut() {
 
 bool isCost(string cost1) {
 	int temp;
-	if (temp = stoi(cost1)) {
-		if (temp > 0)
-			return 1;
+	for (int i = 0; i < cost1.length(); i++) {
+		if (cost1[i] < '0' || cost1[i]>'9') {
+			return 0;
+		}
 	}
+	temp = stoi(cost1);
+	if (temp > 0)
+		return 1;
 	return 0;
 }
 
 bool isDist(string dist1) {
 	int temp;
-	if (temp = stoi(dist1)) {
-		if (temp > 0)
-			return 1;
+	for (int i = 0; i < dist1.length(); i++) {
+		if (dist1[i] < '0' || dist1[i]>'9') {
+			return 0;
+		}
 	}
+	temp = stoi(dist1);
+	if (temp > 0)
+		return 1;
 	return 0;
 }
 
 bool isHour(string hour1) {
 	int temp;
-	if (temp = stoi(hour1)) {
-		if (temp >= 0)
-			return 1;
+	for (int i = 0; i < hour1.length(); i++) {
+		if (hour1[i] < '0' || hour1[i]>'9') {
+			return 0;
+		}
 	}
+	temp = stoi(hour1);
+	if (temp > 0)
+		return 1;
 	return 0;
 }
 
 bool isMinute(string minute1) {
 	int temp;
-	if (temp = stoi(minute1)) {
-		if (temp >= 0 && temp < 60)
-			return 1;
+	for (int i = 0; i < minute1.length(); i++) {
+		if (minute1[i] < '0' || minute1[i]>'9') {
+			return 0;
+		}
 	}
+	temp = stoi(minute1);
+	if (temp >= 0 && temp < 60)
+		return 1;
 	return 0;
 }
 
-void User::bestRoute(vector<Trip> trips, const City& src, const City& arv, int tripType, int method) {
+vector<Trip> User::bestRoute(vector<Trip> trips, const City& src, const City& arv, int tripType, int method) {
 	Graph g;
 	g.init(trips);
-	g.Dijkstra(src, arv, tripType, method);
+	return g.Dijkstra(trips, src, arv, tripType, method);
 }
